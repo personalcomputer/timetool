e60d0f480db42e9707e2669c17e0bd9a8fd0395f
John Miller <personalcomputer@users.noreply.github.com>
2023-05-05 12:08:14 -0700

    add -i mode

--- timetool/main.py
+++ timetool/main.py
@@ -266,23 +266,23 @@ def parse_datetime_core(datetime_agg, log_input_format=False):
     # Warning: Without leading zeros, our logic only supports unix times after **1973-03-03** and up to **33658**
     if re.match(r"^\d{9,12}(?:\.\d+)?$", datetime_agg):
         log_format("unix")
-        return pytz.utc.localize(datetime.datetime.utcfromtimestamp(float(datetime_agg))), input_timezone_raw
+        return datetime.datetime.fromtimestamp(float(datetime_agg), tz=datetime.timezone.utc), input_timezone_raw
     # Or unixime with milliseconds?
     # Warning: Without leading zeros, our logic only supports unix times w/ ms after **2001-09-09** and up to **33658**
     if re.match(r"^\d{13,15}$", datetime_agg):
         log_format("unix milliseconds")
-        return pytz.utc.localize(datetime.datetime.utcfromtimestamp(int(datetime_agg) / 1000.0)), input_timezone_raw
+        return datetime.datetime.fromtimestamp(int(datetime_agg) / 1000.0, tz=datetime.timezone.utc), input_timezone_raw
     # Or unixtime with microseconds?
     # Warning: Without leading zeros, our logic only supports unix times w/ us after **1973-03-03** and up to **5138**
     if re.match(r"^\d{16,18}$", datetime_agg):
         log_format("unix microseconds")
-        return pytz.utc.localize(datetime.datetime.utcfromtimestamp(int(datetime_agg) / 10000000.0)), input_timezone_raw
+        return datetime.datetime.fromtimestamp(int(datetime_agg) / 10000000.0, tz=datetime.timezone.utc), input_timezone_raw
     # Or unixtime with nanoseconds?
     # Warning: Without leading zeros, our logic only supports unix times w/ ns after **2001-09-09** and up to **33658**
     if re.match(r"^\d{19,21}$", datetime_agg):
         log_format("unix nanoseconds")
         return (
-            pytz.utc.localize(datetime.datetime.utcfromtimestamp(int(datetime_agg) / 1000000000.0)),
+            datetime.datetime.fromtimestamp(int(datetime_agg) / 1000000000.0, tz=datetime.timezone.utc),
             input_timezone_raw,
         )
 
@@ -494,7 +494,12 @@ def humanize_time_difference(a_dt, b_dt, variant, relative_to_now_prose=False):
 def run(argv):
     USAGE = textwrap.dedent(
         """
-        Usage: {prog} [-h] [TIME] [in CONVERSION_TIMEZONE] [-o][-e]
+        Usage: {prog} [-h] [TIME] [in CONVERSION_TIMEZONE] [-o][-e][-i]
+
+        Flags:
+            -o  Show "one line" output format that includes all timezones in one line.
+            -e  Show extended delta time formats.
+            -i  Output JUST an ISO8601 UTC time. For repurposing timetool for use in scripts.
 
         Examples:
           {prog}
@@ -516,26 +521,31 @@ def run(argv):
           https://github.com/personalcomputer/timetool/issues
     """
     ).strip()
-    if len(argv) > 1 and argv[1] in ("-h", "--help"):
+    if len(argv) > 1 and (set(argv) & set(["-h", "--help"])):
         print(USAGE.format(prog=argv[0]))
         return
 
-    datetime_agg = " ".join([arg.strip() for arg in argv[1:]])
-    if datetime_agg.endswith("--debug"):
-        datetime_agg = datetime_agg[: -len("--debug")].strip()
+    if "--debug" in argv:
+        argv.remove("--debug")
         debug = True
     else:
         debug = False
-    if datetime_agg.endswith("-e"):
-        datetime_agg = datetime_agg[: -len("-e")].strip()
+    if "-e" in argv:
+        argv.remove("-e")
         extra_output = True
     else:
         extra_output = False
-    if datetime_agg.endswith("-o"):
-        datetime_agg = datetime_agg[: -len("-o")].strip()
+    if "-o" in argv:
+        argv.remove("-o")
         oneline = True
     else:
         oneline = False
+    if "-i" in argv:
+        argv.remove("-i")
+        iso8601_only_mode = True
+    else:
+        iso8601_only_mode = False
+    datetime_agg = " ".join([arg.strip() for arg in argv[1:]])
 
     # Check if the time used 'X in <TIMEZONE>' pattern, and if so then ensure we display that timezone in the output
     match = re.search(r" (?:as|in) ((?:[a-zA-Z/_]{2,},? ?){1,})$", datetime_agg, re.IGNORECASE)
@@ -571,13 +581,15 @@ def run(argv):
             if operator == add:
                 print("Error: Cannot add two datetimes together")
                 sys.exit(1)
-            print(
-                f"{format_datetime_for_inferred_locale(a_dt)} {operator_char} {format_datetime_for_inferred_locale(b_dt)}"
-            )
+            if not iso8601_only_mode:
+                print(
+                    f"{format_datetime_for_inferred_locale(a_dt)} {operator_char} {format_datetime_for_inferred_locale(b_dt)}"
+                )
             handle_delta_display(a_dt, b_dt, extra_output=extra_output)
             return
         # finding absolute..
-        print(f"{format_datetime_for_inferred_locale(a_dt)} {operator_char} {b_str}")
+        if not iso8601_only_mode:
+            print(f"{format_datetime_for_inferred_locale(a_dt)} {operator_char} {b_str}")
         input_time = a_dt + b_delta
         display_prefix = "= "
     else:
@@ -594,6 +606,7 @@ def run(argv):
         input_time,
         display_prefix=display_prefix,
         oneline=oneline,
+        iso8601_only_mode=iso8601_only_mode,
         extra_output=extra_output,
         extra_display_timezones=extra_display_timezones,
     )
@@ -615,8 +628,12 @@ def handle_delta_display(a_dt, b_dt, extra_output):
     print(textwrap.indent("\n".join(output), "= "))
 
 
-def handle_time_display(input_time, display_prefix, oneline, extra_output, extra_display_timezones):
+def handle_time_display(input_time, display_prefix, oneline, iso8601_only_mode, extra_output, extra_display_timezones):
     utc_time = input_time.astimezone(pytz.utc)
+
+    if iso8601_only_mode:
+        print(utc_time.isoformat(timespec="milliseconds"))
+        return
 
     # Unix
     unix_time = input_time.timestamp()
